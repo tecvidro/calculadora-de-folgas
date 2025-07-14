2afd9542525a92bb8f842803e867f220ca2f85ab
thigrandjean <thigrandjean@yahoo.com.br>
2025-07-14 15:41:23 -0300

    fix: variable name for default panels count

--- src/components/CalculatorForm/index.tsx
+++ src/components/CalculatorForm/index.tsx
@@ -1,23 +1,21 @@
-'use client'
+"use client";
 
-import { CalculatorBuilder } from '@/components/Blocks/CalculatorBuilder'
-import { Results } from '@/components/Layout/Results'
-import { Alert } from '@/components/shared/Alert'
-import type { Product } from '@/Types/types'
+import { CalculatorBuilder } from "@/components/Blocks/CalculatorBuilder";
+import { Results } from "@/components/Layout/Results";
+import { Alert } from "@/components/shared/Alert";
+import type { Product } from "@/Types/types";
 
 type CalculatorFormProps = {
-  product: Product
-  alertText: string
-}
+  product: Product;
+  alertText: string;
+};
 
 export const CalculatorForm = ({ product, alertText }: CalculatorFormProps) => {
   return (
     <div className=" flex flex-col items-center gap-4">
-      <h1 className="w-full text-center uppercase">{product.name}</h1>
-      <p className="w-full text-center">{product.description}</p>
       <Alert text={alertText} />
       <CalculatorBuilder content={product.content} />
       <Results product={product.name} />
     </div>
-  )
-}
+  );
+};
