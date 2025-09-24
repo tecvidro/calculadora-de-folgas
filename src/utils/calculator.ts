199af9f0a8c09b67be4844aa1b184f0625422903
thigrandjean <thigrandjean@yahoo.com.br>
2025-09-24 09:03:42 -0300

    feat: calculator comments

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -21,26 +21,26 @@ export const calculatorVdpl = ({ params }: Calculator) => {
   const totalGlassesCount = panelsCount + doorsCount
 
   const adjustments = {
-    profile: 19,
-    transpass: 15,
-    doorAdjustment: 31,
-    heightDiscount: 85,
+    profile: 19, // desconto perfil
+    transpass: 15, // transpasse
+    doorAdjustment: 31, // ajuste das portas
+    heightDiscount: 85, // ajuste altura perfis
   }
 
-  const totalProfileDiscount = adjustments.profile * 2
-  const totalTranpass = adjustments.transpass * (totalGlassesCount - 1)
-  const lockDiscounts = params.lock.reduce((sum, current) => sum + current, 0)
+  const totalProfileDiscount = adjustments.profile * 2 // multiplicar desconto perfis por 2
+  const totalTranpass = adjustments.transpass * (totalGlassesCount - 1) // calcular total de transpasses
+  const lockDiscounts = params.lock.reduce((sum, current) => sum + current, 0) // somar todos os descontos de fechadura
 
   const totalGap =
-    gapWidth - totalProfileDiscount + (totalTranpass - lockDiscounts)
+    gapWidth - totalProfileDiscount + (totalTranpass - lockDiscounts) // calcular o vão total com os descontos
 
-  const finalHeight = Math.floor(gapHeight - adjustments.heightDiscount)
+  const finalHeight = Math.floor(gapHeight - adjustments.heightDiscount) // calcular altura
 
-  const panelsWidth = Math.floor(totalGap / totalGlassesCount)
+  const panelsWidth = Math.floor(totalGap / totalGlassesCount) // calcular a largura dos painéis (vão / nº total de vidros)
 
   const doorsWidths = Array.from({ length: doorsCount }, (_, index) => {
     const doorWidth =
-      panelsWidth + params.lock[index] - adjustments.doorAdjustment
+      panelsWidth + params.lock[index] - adjustments.doorAdjustment // calcular a largura das portas (lagura painel + desconto fechadura - ajustes fas portas)
     return { id: index + 1, width: Math.floor(doorWidth) }
   })
 
@@ -56,7 +56,6 @@ export const calculatorVdpo = ({ params }: Calculator) => {
   const gapHeight = params.gap.height
   const panelsCount = params.glasses.panels
   const doorsCount = params.glasses.doors
-
   const totalGlassesCount = panelsCount + doorsCount
 
   const adjustments = {
@@ -68,22 +67,22 @@ export const calculatorVdpo = ({ params }: Calculator) => {
     throw new Error('ERRD48O')
   }
 
-  const totalProfileDiscount = doorsCount === 2 ? 10 : 16
+  const totalProfileDiscount = doorsCount === 2 ? 10 : 16 // desconto perfis
 
-  const totalTranpass = adjustments.transpass * (panelsCount + 2)
+  const totalTranpass = adjustments.transpass * (panelsCount + 2) // calcular total de transpasses (transpasse * total de painéis + 2)
 
-  const lockDiscounts = params.lock.reduce((sum, current) => sum + current, 0)
+  const lockDiscounts = params.lock.reduce((sum, current) => sum + current, 0) // calcular total de desconto das fechaduras
 
   const totalGap =
-    gapWidth - totalProfileDiscount + totalTranpass - lockDiscounts
+    gapWidth - totalProfileDiscount + totalTranpass - lockDiscounts // calcular vão total
 
-  const finalHeight = Math.floor(gapHeight - adjustments.heightDiscount)
+  const finalHeight = Math.floor(gapHeight - adjustments.heightDiscount) // calcular altura com descontos
 
-  const panelsWidth = Math.floor(totalGap / totalGlassesCount)
+  const panelsWidth = Math.floor(totalGap / totalGlassesCount) // calcular a largura dos painéis (vão total / nº total de vidros)
 
   const doorsWidths = Array.from({ length: doorsCount }, (_, index) => {
-    const lockValue = params.lock[index] ?? 0
-    const doorWidth = panelsWidth + lockValue
+    const lockValue = params.lock[index] ?? 0 // pega o valor do desconto de fechadura
+    const doorWidth = panelsWidth + lockValue // calcula a largura das portas (largura dos painéis + desonto da fechadura)
     return { id: index + 1, width: Math.floor(doorWidth) }
   })
 
